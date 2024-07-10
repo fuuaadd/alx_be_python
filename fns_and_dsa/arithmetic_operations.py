4ace6e650e2c5d9b9d89024884d370c870f07811
fuad <fuadbinseid@gmail.com>
2024-07-10 22:30:18 +0300

    function 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,33 @@
+# def perform_operation(num1, num2, operation):
+#     match operation:
+#         case "add":
+#             result = num1+num2
+#             return result
+#         case "subtract" :
+#             result = num1 - num2
+#         case "multiply":
+#             result  = num1 * num2
+#             return result
+#         case "divide":
+#             if num2 == 0:
+#                 result ="invalid operation"
+#             else :
+#                 result = num1/num2
+#             return result
+#         case _:
+#             return print("invalid symbol")
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            result = num1+num2
-            return result
-        case "subtract" :
-            result = num1 - num2
-        case "multiply":
-            result  = num1 * num2
-            return result
-        case "divide":
-            if num2 == 0:
-                result ="invalid operation"
-            else :
-                result = num1/num2
-            return result
-        case _:
-            return print("invalid symbol")
-
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
      
