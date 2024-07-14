66d3e7684c3c448fe0513da1cdcbdc2147860157
fuad <fuadbinseid@gmail.com>
2024-07-14 11:17:14 +0300

    bank

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,16 @@
 class BankAccount:
-    account_balance= 0
-    def __init__(self, initial_balance=0) -> None:
-        pass
-    def deposit(self, amount):
-        self.amount = amount
+    def __init__(self,initial_balance=0):
+        self.account_balance = initial_balance
+    
+    def deposit(self,amount):
         self.account_balance += amount
-        return self.account_balance
     def withdraw(self, amount):
-        self.amount = amount
-        if (self.account_balance - amount) >= 0:
-            self.account_balance = self.account_balance - amount
-    def display_balance():
-        pass
+        if (self.account_balance > 0) and (amount <= self.account_balance):
+            self.account_balance -= amount
+            return True 
+        return False
+    def display_balance(self):
+        print(f"current balance: ${self.account_balance}")
+    
+
 
